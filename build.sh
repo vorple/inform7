fe60fd6ab99f086a1a9a52e566724605c290b530
Juhana Leinonen <git@nitku.net>
2022-06-26 20:01:11 +0300

    Correct paths in the build script

--- build.sh
+++ build.sh
@@ -7,10 +7,10 @@ mkdir dist
 cp -r Extensions Templates dist
 
 # We assume that the main Vorple repository is in ../vorple and it's already been built
-cp ../vorple/dist/interpreter/* dist/Templates/Vorple
+cp ../vorple/web/interpreter/* dist/Templates/Vorple
 
 # Put the list of interpreter files into the manifest file
-find ../vorple/dist/interpreter/ -type f -exec basename {} \; >> dist/Templates/Vorple/\(manifest\).txt
+find ../vorple/web/interpreter/ -type f -exec basename {} \; >> dist/Templates/Vorple/\(manifest\).txt
 
 
 cd dist
